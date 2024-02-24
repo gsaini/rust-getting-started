635858a3f0461031e2e25947f9902d18d37c8219
Gopal Saini <gopal.saini.work@gmail.com>
2024-02-24 19:44:15 +0000

    removed build target files.

--- projects/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust-getting-started/projects/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499: src/main.rs
-
-/workspaces/rust-getting-started/projects/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499.d: src/main.rs
-
-src/main.rs:
--- projects/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust-getting-started/projects/hello_cargo/target/debug/hello_cargo: /workspaces/rust-getting-started/projects/hello_cargo/src/main.rs
